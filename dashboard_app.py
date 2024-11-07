484c78a0e60a7fa36189f8837a6a11d40f84bc44
Richard-CG <richardchees7@gmail.com>
2024-11-07 23:50:09 +0000

    release version 1.0

--- dashboard_app.py
+++ dashboard_app.py
@@ -6,15 +6,8 @@ import json
 
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
-db = firestore.Client(credentials=creds, project="reto-dashboard-6ad18")
-movies_ref = db.collection("movies")
 
-docs = movies_ref.stream()
 
-# Convertir los documentos en una lista de diccionarios
-movies_data = [doc.to_dict() for doc in docs]
-st.write(pd.DataFrame(movies_data))
-print(movies_data)
 
 st.title('Streamlit con atributo cache')
 
@@ -22,13 +15,20 @@ st.title('Streamlit con atributo cache')
 @st.cache_data
 def load_data(nrows=None):
 
-    df = pd.DataFrame(movies_data)
+  db = firestore.Client(credentials=creds, project="reto-dashboard-6ad18")
+  movies_ref = db.collection("movies")
+  docs = movies_ref.stream()
+    
+    # Convertir los documentos en una lista de diccionarios
+  movies_data = [doc.to_dict() for doc in docs]
+  df = pd.DataFrame(movies_data)
     
     # Limitar el número de filas si se especifica
-    if nrows:
-        df = df.head(nrows)
+  if nrows:
+
+    df = df.head(nrows)
     
-    return df
+  return df
 
 # Estado de carga
 data_load_state = st.text("Cargando datos...")
